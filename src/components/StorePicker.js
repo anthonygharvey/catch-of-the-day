877308816077430b3247c2d8bc2bb5499d336bcb
anthonygharvey <anthony@anthonygharvey.com>
2019-02-04 00:22:23 -0500

    Added StorePicker component form and imported app css

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,6 +2,12 @@ import React from "react";
 
 export default class StorePicker extends React.Component {
   render() {
-    return <div>I am the StorePicker component!</div>;
+    return (
+      <form action="" className="store-selector">
+        <h2>Please Enter A Store</h2>
+        <input type="text" name="" id="" placeholder="Store Name" />
+        <button type="submit">Visit Store -></button>
+      </form>
+    );
   }
 }
