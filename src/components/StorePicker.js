4b5786a351729ea2b2c9bfdda064f90f8a861e86
anthonygharvey <anthony@anthonygharvey.com>
2019-02-10 09:31:27 -0500

    Added onSubmit event to StorePicker

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,14 +1,25 @@
 import React from "react";
 import { getFunName } from "../helpers";
 export default class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = event => {
+    // 1. Stop the form from submitting
+    event.preventDefault();
+    // 2. Get the text from that input
+    const storeName = this.myInput.current.value;
+    // 3. Change the page to /store/whatever-they-entered
+    this.props.history.push(`/store/${storeName}`);
+  };
+
   render() {
     return (
-      <form action="" className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
         <input
           type="text"
-          name=""
-          id=""
+          ref={this.myInput}
+          required
           placeholder="Store Name"
           defaultValue={getFunName()}
         />
