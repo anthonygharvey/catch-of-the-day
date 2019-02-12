19fc98d6f002c19c83a3ef1097b9a35520795d61
anthonygharvey <anthony@anthonygharvey.com>
2019-02-11 20:20:21 -0500

    Added AddFishForm and updated App State

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,23 @@ import Inventory from "./Inventory";
 import Order from "./Order";
 
 export default class App extends Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = fish => {
+    // 1. Take a copy of the existing state
+    const fishes = { ...this.state.fishes }; //object spread.  Deep cloning isn't necessary.
+    // 2. Add our new fish to that fishes variable
+    fishes[`fish${Date.now()}`] = fish;
+    // 3. Set the new fishes object to state
+    this.setState({
+      // fishes: fishes
+      fishes //in ES6, if your property and value are the same, you can just pass in the property
+    });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +28,7 @@ export default class App extends Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     );
   }
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
+import AddFishForm from "./AddFishForm";
 
 export default class Inventory extends Component {
   render() {
-    return <div className="Inventory">Inventory!!</div>;
+    return (
+      <div className="Inventory">
+        <h2>Inventory</h2>
+        <AddFishForm addFish={this.props.addFish} />
+      </div>
+    );
   }
 }
